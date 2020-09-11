43d38f4fdcd89957318f3f5e2e4923413a9800cb
babyhir <loyal_mav89@hotmail.com>
2020-09-11 10:39:20 +0800

    Updates

---
+++ precache-manifest.2135a33ba2f670047497a0fab6cd7491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d748dab11f6e697510111c333984a7",
+    "url": "/store-react/index.html"
+  },
+  {
+    "revision": "98381ae37a5886736f23",
+    "url": "/store-react/static/css/main.706448ca.chunk.css"
+  },
+  {
+    "revision": "019e6ce9e4a826ae94f1",
+    "url": "/store-react/static/js/2.b85d08bb.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/store-react/static/js/2.b85d08bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98381ae37a5886736f23",
+    "url": "/store-react/static/js/main.e66684db.chunk.js"
+  },
+  {
+    "revision": "a9c02f3cd904aeef5dc7",
+    "url": "/store-react/static/js/runtime-main.383c2e74.js"
+  }
+]);
